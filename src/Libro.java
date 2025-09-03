708951b7a10f85e16da0d58561ffb0e073e25256
Sebastian Acuña <sebsg1806@gmail.com>
2025-09-02 19:24:19 -0500

    se ajustaron variables y se inicio el metodo constructor con las variables titulo y autor y los get y set de todas las variables

--- src/Libro.java
+++ src/Libro.java
@@ -1,11 +1,59 @@
 public class Libro {
 
-    public String titulo;
-    public String Autor;
-    public String numeroEjemplares;
-    public String numeroEjemplaresPrestados;
+    private String titulo;
+    private String Autor;
+    private int numeroEjemplares = 0;
+    private int numeroEjemplaresPrestados = 0;
 
     
+    private Libro(String titulo, String autor) {
+        this.titulo = titulo;
+        Autor = autor;
+    }
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+
+    public String getAutor() {
+        return Autor;
+    }
+
+
+    public void setAutor(String autor) {
+        Autor = autor;
+    }
+
+
+    public int getNumeroEjemplares() {
+        return numeroEjemplares;
+    }
+
+
+    public void setNumeroEjemplares(int numeroEjemplares) {
+        this.numeroEjemplares = numeroEjemplares;
+    }
+
+
+    public int getNumeroEjemplaresPrestados() {
+        return numeroEjemplaresPrestados;
+    }
+
+
+    public void setNumeroEjemplaresPrestados(int numeroEjemplaresPrestados) {
+        this.numeroEjemplaresPrestados = numeroEjemplaresPrestados;
+    }
+
+    
+
+
     
 
 
